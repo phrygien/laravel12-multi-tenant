e42478c9c345578db639fdad710eb4f16ac24ddb
Mecene Phrygien <mecenephrygien@gmail.com>
2025-02-27 22:02:41 +0300

    sidebar

--- resources/views/livewire/admins/modules/index.blade.php
+++ resources/views/livewire/admins/modules/index.blade.php
@@ -7,5 +7,6 @@ new class extends Component {
 }; ?>
 
 <div>
-    //
+    <flux:heading level="3" size="xl">Gerer les modules</flux:heading>
+    <flux:subheading>This information will be displayed publicly.</flux:subheading>
 </div>
